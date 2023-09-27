3dc46dd992838aaad1ec306373de3cba6f6da6dc
th3888 <hotake05@gmail.com>
2023-09-27 23:59:28 +0900

    ex ac 2

--- abc/abc321/b.py
+++ abc/abc321/b.py
@@ -1,16 +1,21 @@
-N, X = map(int, input().split())
-A = list(map(int, input().split()))
+N,X = map(int,input().split())
+A = list(map(int,input().split()))
 A.append(-1)
 
-for last in range(0, 101):
-    B = A.copy
-    B[N - 1] = last
-    B.sort()
-    total = 0
-    for i in range(1, N - 1):
-        total += B[i]
-    if total >= X:
+for last in range(0,101):
+    B = A.copy()
+    B[N-1] = last
+    sum = 0
+    ma = 0
+    mi = 100
+    for p in B:
+        sum += p
+        ma = max(ma,p)
+        mi = min(mi,p)
+    score = sum - ma - mi
+
+    if score >= X:
         print(last)
         exit()
-
-print('-1')
+    
+print("-1")
