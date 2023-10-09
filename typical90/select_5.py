05c2367f84837e2ceb2c0838f0ede774a2601edf
th3888 <hotake05@gmail.com>
2023-10-09 13:24:02 +0900

    revise

--- typical90/select_5.py
+++ typical90/select_5.py
@@ -3,11 +3,11 @@ A = list(map(int, input().split()))
 
 ans = 0
 
-for i in range(N + 1):
-    for j in range(N + 1):
-        for k in range(N + 1):
-            for l in range(N + 1):
-                for m in range(N + 1):
+for i in range(0, N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            for l in range(k + 1, N):
+                for m in range(l + 1, N):
                     if ((((A[i] * A[j] % P) * A[k] % P) * A[l] % P) * A[m]) == Q:
                         ans += 1
 
