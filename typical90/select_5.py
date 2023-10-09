ddd6824d71caf11812ddd4b8d58e1ee0b098a523
th3888 <hotake05@gmail.com>
2023-10-09 13:29:32 +0900

    revise

--- typical90/select_5.py
+++ typical90/select_5.py
@@ -8,7 +8,7 @@ for i in range(0, N):
         for k in range(j + 1, N):
             for l in range(k + 1, N):
                 for m in range(l + 1, N):
-                    if ((((A[i] * A[j] % P) * A[k] % P) * A[l] % P) * A[m]) == Q:
+                    if ((((A[i] * A[j] % P) * A[k] % P) * A[l] % P) * A[m]) % P == Q:
                         ans += 1
 
 print(ans)
