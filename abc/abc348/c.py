7595edb1d693a047b34ed574badab84896419aad
th3888 <hotake05@gmail.com>
2024-04-07 11:25:20 +0900

    ex AC

--- abc/abc348/c.py
+++ abc/abc348/c.py
@@ -1,8 +1,17 @@
 from collections import defaultdict
 
 N = int(input())
-ac = [map(int, input().split()) for _ in range(N)]
-a, c = [list(i) for i in zip(*ac)]
+ac = [list(map(int, input().split())) for _ in range(N)]
 
-taste = defaultdict(int)
+inf = float('INF')
+taste = defaultdict(lambda: inf)
 
+for a, c in ac:
+    taste[c] = min(taste[c], a)
+
+ans = 0
+
+for min_a in taste.values():
+    ans = max(min_a, ans)
+
+print(ans)
